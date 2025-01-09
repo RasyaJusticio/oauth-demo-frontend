8e5117491fd6f01939d38af3aead60aee6764736
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-09 16:30:15 +0700

    feat: implement optional sign-in with Google

---
+++ src/routes/(guest)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let { children } = $props();
+
+	$effect(() => {
+		if (localStorage.getItem('isLoggedIn')) {
+			goto('/');
+		}
+	});
+</script>
+
+{@render children()}
---
+++ src/routes/(guest)/auth/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+
+	$effect(() => {
+		const authStatus = page.url.searchParams.get('status');
+        
+		if (authStatus === 'success') {
+            localStorage.setItem('isLoggedIn', 'true');
+			goto('/');
+		} else {
+            localStorage.removeItem('isLoggedIn');
+			goto('/sign-in');
+		}
+	});
+</script>
