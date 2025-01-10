7538bf3379c547bc2a79159f360b6551ae87641a
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-10 10:01:27 +0700

    feat: add logout functionality with navigation

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import axiosClient from '$lib/axios';
 
 	let { children } = $props();
 	let isLoggedIn = $state(false);
@@ -11,8 +12,26 @@
 			isLoggedIn = true;
 		}
 	});
+
+	async function logout() {
+		try {
+			await axiosClient.post('auth/logout');
+		} catch (error) {
+			//
+		} finally {
+			localStorage.removeItem('isLoggedIn');
+			goto('/sign-in');
+		}
+	}
 </script>
 
 {#if isLoggedIn}
+	<header class="sticky top-0 z-50 flex items-center bg-blue-500 px-4 py-3 text-white">
+		<a href="/">
+			<h1 class="text-xl font-bold">OAuth Demo</h1>
+		</a>
+
+		<button class="ml-auto hover:text-neutral-200" onclick={logout}> Logout </button>
+	</header>
 	{@render children()}
 {/if}
