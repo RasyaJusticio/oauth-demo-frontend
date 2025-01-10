ea35e5a43d1a233e0a56b06d2ae0c70b8d1217ea
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-10 10:01:40 +0700

    feat: add Google authentication code verification

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
+axios.defaults.withCredentials = true;
 const axiosClient = axios.create({
 	baseURL: `${PUBLIC_BACKEND_URL}/api/`,
 	headers: {
--- src/routes/(guest)/auth/+page.svelte
+++ src/routes/(guest)/auth/+page.svelte
@@ -1,16 +1,26 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import axiosClient from '$lib/axios';
 
-	$effect(() => {
-		const authStatus = page.url.searchParams.get('status');
+	async function verifyAuthCode() {
+		const authCode = page.url.searchParams.get('code');
         
-		if (authStatus === 'success') {
-            localStorage.setItem('isLoggedIn', 'true');
-			goto('/');
+		if (authCode) {
+			try {
+				await axiosClient.post('auth/google/exchange', { code: authCode });
+				localStorage.setItem('isLoggedIn', 'true');
+				goto('/');
+			} catch (error) {
+				console.error(error)
+				goto('/sign-in');
+			}
 		} else {
-            localStorage.removeItem('isLoggedIn');
 			goto('/sign-in');
 		}
+	}
+
+	$effect(() => {
+		verifyAuthCode();
 	});
 </script>
